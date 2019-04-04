073c1b8d0558a3f3b041384f77c3dc85eec7df78
kasem <kasem.w@dzcard.com>
2019-04-04 10:10:43 +0700

    Added MyForm

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 // import Login from './components/Login';
 import Register from './components/Register';
+import MyForm from './components/MyForm';
 
-ReactDOM.render(<Register />, document.getElementById('root'));
+ReactDOM.render(<MyForm />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
