9201d6d0a2d6b1c7be885035511fafd237500c32
kasem <kasem.w@dzcard.com>
2019-04-11 09:36:36 +0700

    can detect change all control

--- src/components/FormValidation.js
+++ src/components/FormValidation.js
@@ -7,10 +7,11 @@ export default class FormValidation extends Component{
 
         this.state={
             newUser:{
-                firstname:'',
-                age:'',
-                gender:'',
+                firstname:null,
+                age:null,
+                gender:null,
                 skills:[],
+                about:null
             },
             genders:["Male","Female","Orther"],
             optionSkills:["Programmer","Development","Testing","Design","UX-UI"]
@@ -34,18 +35,23 @@ export default class FormValidation extends Component{
     }
 
     handleCheckbox=(e)=>{
-        // console.log(this.state.newUser.skill);
-        alert(e.target.value);
+        
         const newSelection = e.target.value;
         let newSelectionArray;
+        
+        if (this.state.newUser.skills.indexOf(newSelection) > -1) {
+            newSelectionArray = this.state.newUser.skills.filter(skill => skill !== newSelection);
+        } else {
+            newSelectionArray = [...this.state.newUser.skills, newSelection];
+        }
 
-        newSelectionArray = [...this.state.newUser.skills,newSelection];
+        this.setState(prevState => ({newUser: { ...prevState.newUser, skills: newSelectionArray }}));
+    }
 
-        this.setState(prevState => ({
-            newUser: { ...prevState.newUser, skills: newSelectionArray }
-          }));
+    handleTextArea=(e)=> {
+        let value = e.target.value;
 
-
+        this.setState(prevState => ({newUser:{...prevState.newUser,about:value}}));
     }
 
     render(){
@@ -55,14 +61,17 @@ export default class FormValidation extends Component{
                 <div className="row">
                     <div className="col-md-6">
                         <form onSubmit={this.handleSubmit}>
+
                             <div className="form-group">
                                 <label htmlFor="fullname">Full Name</label>
                                 <input type="text" name="firstname" onChange={this.handleChange} className="form-control" id="fullname" aria-describedby="fullnameHelp" placeholder="Enter your name" />
                             </div>
+
                             <div className="form-group">
                                 <label htmlFor="age">Age</label>
                                 <input type="text" name="age" onChange={this.handleChange} className="form-control" id="age" placeholder="Enter your age" />
                             </div>
+
                             <div className="form-group">
                                 <label htmlFor="gender">Gender</label>
                                 <select id="gender" name="gender" value={this.state.newUser.gender} onChange={this.handleChange} className="form-control">
@@ -79,29 +88,36 @@ export default class FormValidation extends Component{
                                 {this.state.optionSkills.map(skill => {
                                     return (
                                         <div className="form-check form-check-inline">
-                                            <input 
-                                                id="skills"
-                                                name="skills"
-                                                value={skill}
-                                                onChange={this.handleCheckbox} 
-                                                //checked={this.state.newUser.skills.indexof(skill) > -1}
-                                                checked={this.state.newUser.skills.indexOf(skill) > -1}
-                                                className="form-check-input"
-                                                type="checkbox" 
-                                                />
-                                            {/* <label className="form-check-label" htmlFor="gridCheck">
+                                            <label className="form-check-label" htmlFor="gridCheck">
                                                 {skill}
-                                            </label> */}
+                                                <input
+                                                    id="skills"
+                                                    name="skills"
+                                                    value={skill}
+                                                    onChange={this.handleCheckbox}
+                                                    checked={this.state.newUser.skills.indexOf(skill) > -1}
+                                                    className="form-check-input"
+                                                    type="checkbox" />
+                                            </label>
                                         </div>
                                     )
                                 }
-                                )}
+                            )}
                             </div>
 
                             <div className="form-group">
                                 <label htmlFor="about">About you</label>
-                                <textarea className="form-control" id="about" rows="4"></textarea>
+                                <textarea 
+                                    className="form-control"
+                                    id="about"
+                                    name="about"
+                                    rows="4"
+                                    cols="3"
+                                    value={this.state.newUser.about}
+                                    onChange={this.handleTextArea}
+                                />
                             </div>
+
                             <button type="submit" className="btn btn-primary">Submit</button>
                         </form>
                     </div>
